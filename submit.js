518df4b9f27a9256c9faba91a70ff26c01f82d5f
S M Hossain Mazba Alvi <85153673+Alvi100@users.noreply.github.com>
2023-08-13 18:16:10 +0600

    submit.js

--- submit.js
+++ submit.js
@@ -1,6 +1,6 @@
 function myFunction(){
    document.getElementById("name").innerHTML= prompt("Enter Your Name: [ max-22 Character ] ");
-   document.getElementById("roll").innerHTML= prompt("Enter Your Roll: [ last 2 or 3 numbers ] ");
+   document.getElementById("roll").innerHTML= prompt("Enter Your Roll: [ last 3 numbers, Example: 001 ] ");
    document.getElementById("sname").style.display="none";
    
 }
